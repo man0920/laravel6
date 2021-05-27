9d8ae7a2735559d104a22bac4f4797a35ca4713e
Manju Kumari <manjukumari6719@gmail.com>
2021-05-27 12:36:36 +0530

    Layout Pages (Episode-14)

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,9 @@
 
 
 
-  Route::get('posts/{post}', 'PostsController@show');
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
