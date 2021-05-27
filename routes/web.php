05f664cff90e947dcbd5052093adb096e801512e
Manju Kumari <manjukumari6719@gmail.com>
2021-05-27 13:27:09 +0530

    Integrate a Site template (Episode-15)

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/contact', function () {
-    return view('contact');
+Route::get('/about', function () {
+    return view('about');
 });
