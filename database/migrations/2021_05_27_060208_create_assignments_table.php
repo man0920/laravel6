7503af0d6093e099e8d70c45a087883a3f936c4a
Manju Kumari <manjukumari6719@gmail.com>
2021-05-27 11:49:30 +0530

    Business Logic (Episode-13)

--- database/migrations/2021_05_27_060208_create_assignments_table.php
+++ database/migrations/2021_05_27_060208_create_assignments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProjectsTable extends Migration
+class CreateAssignmentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,12 @@ class CreateProjectsTable extends Migration
      */
     public function up()
     {
-        Schema::create('projects', function (Blueprint $table) {
+        Schema::create('assignments', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->text('body');
+            $table->boolean('completed_at')->default(false);
             $table->timestamps();
+            $table->timestamp('due_date')->nullable();
         });
     }
 
@@ -26,6 +29,6 @@ class CreateProjectsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('projects');
+        Schema::dropIfExists('assignments');
     }
 }
